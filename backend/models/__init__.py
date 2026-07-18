3371849cfd09bf32167dd9aa9b8ed43d864de46a
Bruno de Sousa <brunodesousa.ti@gmail.com>
2026-07-17 22:14:12 -0300

    feat: update models init and fix models content

---
+++ backend/models/__init__.py
@@ -0,0 +1,4 @@
+from models.user import User
+from models.client import Client
+from models.sale import Sale
+from models.sale_item import SaleItem
--- backend/models/sale_item.py
+++ backend/models/sale_item.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
 from sqlalchemy.orm import relationship
 
 from database import Base
